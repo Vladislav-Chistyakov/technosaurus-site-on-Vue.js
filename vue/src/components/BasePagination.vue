e5295dadcc85877b15cd306d028cbee9c2b9d567
Vladislav_Chistyakov <vladischis@gmail.com>
2022-11-29 15:52:18 +0500

    add paginate arrows

--- vue/src/components/BasePagination.vue
+++ vue/src/components/BasePagination.vue
@@ -1,8 +1,10 @@
 <template>
 <ul class="catalog__pagination pagination">
   <li class="pagination__item">
-    <a class="pagination__link pagination__link--arrow pagination__link--disabled"
-      aria-label="Предыдущая страница">
+    <a @click.prevent="paginateBack(page)"
+    class="pagination__link pagination__link--arrow"
+    :class="{'pagination__link--disabled': page === 1}"
+    aria-label="Предыдущая страница" href="#">
       <svg width="8" height="14" fill="currentColor">
         <use xlink:href="#icon-arrow-left"></use>
       </svg>
@@ -15,7 +17,9 @@
     </a>
   </li>
   <li class="pagination__item">
-    <a class="pagination__link pagination__link--arrow"
+    <a @click.prevent="paginateForward(page, pages)"
+    :class="{'pagination__link--disabled': page === pages}"
+      class="pagination__link pagination__link--arrow"
       href="#" aria-label="Следующая страница">
       <svg width="8" height="14" fill="currentColor">
         <use xlink:href="#icon-arrow-right"></use>
@@ -39,7 +43,18 @@ export default {
   },
   methods: {
     paginate(page) {
+      console.log(page);
       this.$emit('paginate', page);
+    },
+    paginateForward(page, pages) {
+      if (page !== pages) {
+        this.$emit('paginate', page + 1);
+      }
+    },
+    paginateBack(page) {
+      if (page !== 1) {
+        this.$emit('paginate', page - 1);
+      }
     },
   },
 };
