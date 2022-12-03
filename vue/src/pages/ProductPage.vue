54dabb836966263e05764bed4ecc32e16a5841aa
Vladislav_Chistyakov <vladischis@gmail.com>
2022-12-03 15:17:21 +0500

    add color in pages

--- vue/src/pages/ProductPage.vue
+++ vue/src/pages/ProductPage.vue
@@ -41,25 +41,20 @@
             <fieldset class="form__block">
               <legend class="form__legend">Цвет:</legend>
               <ul class="colors">
-                <li class="colors__item">
+                <!--<li class="colors__item" v-for="colorTheme in product.color" :key="colorTheme.colorId">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="blue" checked="">
-                    <span class="colors__value" style="background-color: #73B6EA;">
+                    <input class="colors__radio sr-only" type="radio"
+                    :value="colorTheme.colorHesh" v-model="color" >
+                    <span class="colors__value" :style="{ backgroundColor: colorTheme.colorHesh, }">
                     </span>
                   </label>
-                </li>
-                <li class="colors__item">
+                </li>-->
+                <li class="colors__item" v-for="colorTheme in product.color" :key="colorTheme.colorId">
                   <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="yellow">
-                    <span class="colors__value" style="background-color: #FFBE15;">
+                    <input class="colors__radio sr-only" type="radio" name="color-item" :value="colorTheme.colorHesh" v-model="color">
+                    <span class="colors__value" :style="{ backgroundColor: colorTheme.colorHesh }">
                     </span>
                   </label>
-                </li>
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input class="colors__radio sr-only" type="radio" name="color-item" value="gray">
-                    <span class="colors__value" style="background-color: #939393;">
-                  </span></label>
                 </li>
               </ul>
             </fieldset>
