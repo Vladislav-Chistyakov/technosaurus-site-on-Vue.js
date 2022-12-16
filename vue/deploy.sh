5547f4126dec276f0d4ea8838fae22f41055b0ee
Vladislav_Chistyakov <vladischis@gmail.com>
2022-12-17 04:20:31 +0500

    deploy and readme

--- vue/deploy.sh
+++ vue/deploy.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 
-# Остановить публикацию при ошибках
-set -env
-
 # сборка приложения
 npm run build
 
 # переход в каталог сборки
 cd dist
 
+# создание README.md
+echo "###Для просмотра проекта перейдите по ссылке https://vladislav-chistyakov.github.io/vue-app-tehnozavr" > README.md
+
 # инициализация репозиторий и загрузка кода на GitHub
 git init
 git add .
 git commit -m "deploy"
 
-git push -f https://github.com/Vladislav-Chistyakov/vue-tehnosaurus.github.io master:gh-pages
+git push -f https://github.com/Vladislav-Chistyakov/vue-app-tehnozavr.git master:gh-pages
 
-cd -
-
+#cd -
